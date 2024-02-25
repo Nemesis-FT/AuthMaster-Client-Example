69b8bfe13de2aba536b98d0abb50bf73486d5a2a
nemesis <lorenzo.balugani@gmail.com>
2024-02-25 11:55:16 +0100

    :sparkles: Fix broken dep

--- backend/__main__.py
+++ backend/__main__.py
@@ -4,8 +4,10 @@ import bcrypt
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, APIKeyHeader
 from fastapi_oauth2.claims import Claims
+from fastapi_oauth2.security import OAuth2
+from starlette.requests import Request
 
 from backend.authentication import AuthMasterOAuth2
 from backend.database import models
@@ -18,8 +20,6 @@ from backend.configuration import setting_required
 from backend.errors import *
 from backend.handlers import *
 from backend.configuration import JWT_KEY, OAUTH2_CLIENT, OAUTH2_SECRET
-from social_core.backends.oauth import BaseOAuth2
-from social_core.backends.gitlab import GitLabOAuth2
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -48,13 +48,30 @@ app.add_middleware(OAuth2Middleware, config=OAuth2Config(
             backend=AuthMasterOAuth2,
             client_id=OAUTH2_CLIENT,
             client_secret=OAUTH2_SECRET,
-            redirect_uri="http://google.it",
+            redirect_uri="http://127.0.0.1:8001/test",
             scope=["Profile"],
-            claims=Claims(username=lambda u: u.username)
+            claims=Claims(username=lambda u: u.username, email=lambda u: u.email, id=lambda u: u.id)
         )
     ]
 ), callback=test)
 
+
+@app.get("/test")
+async def test(request: Request):
+    return {"token": request.cookies.get("Authorization"), "token2": request.auth.provider.access_token,
+            "user": request.user}
+
+
+oauth = OAuth2()
+bearer = APIKeyHeader(name='Authorization', scheme_name='authorization')
+
+
+@app.get("/test2")
+async def test2(request: Request, auth=Depends(oauth), bearer=Depends(bearer)):
+    return {"token": request.cookies.get("Authorization"),
+            "user": request.user}
+
+# On authmaster, set redirect_uri to http://127.0.0.1:8001/oauth2/AuthMaster/token!
 
 app.add_exception_handler(AcmedeliverException, handle_acme_error)
 app.add_exception_handler(sqlalchemy.exc.NoResultFound, handle_sqlalchemy_not_found)
